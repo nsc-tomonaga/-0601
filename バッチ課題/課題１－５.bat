447eff60cbdebda3aaa765b649db60f3e60ce8cb
nsc-tomonaga <63280570+nsc-tomonaga@users.noreply.github.com>
2020-06-01 12:51:28 +0900

    バッチ課題0602

---
+++ バッチ課題/課題１－５.bat
@@ -0,0 +1,8 @@
+echo "hello world"
+cd
+dir /B
+cd ..
+cd downloads
+dir /B *>> ..\Desktop\downloads.txt
+ipconfig
+pause
---
+++ バッチ課題/課題６.bat
@@ -0,0 +1,7 @@
+cd desktop
+rename a.txt ファイル1.txt
+rename b.txt ファイル2.txt
+rename c.txt ファイル3.txt
+rename d.txt ファイル4.txt
+rename e.txt ファイル5.txt
+pause
